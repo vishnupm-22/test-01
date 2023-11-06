8dc1cd799e0420806610b2c2bf13b7481c250bd9
vishnupm-22 <142531399+vishnupm-22@users.noreply.github.com>
2023-11-06 23:34:58 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pickle
 import string
 import nltk
 nltk.download('punkt')
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
